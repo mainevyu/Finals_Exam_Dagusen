b9799b2eb5bfde689725fac6922502b6377108d0
Charmaine D. Dagusen <20255342@s.ubaguio.edu>
2026-05-11 10:37:13 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <NavigationBar />
     
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
